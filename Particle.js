0ce6b5e62f398a199ba5d6d9fb69247df0c0d1a5
Zack Stout <zackstout@gmail.com>
2018-04-01 23:40:02 -0500

    Oh, it seems now that attraction is working along both dimensions

--- Particle.js
+++ Particle.js
@@ -18,29 +18,29 @@ class Particle {
   }
 
   getForce(particle) {
-    // get distance between particles:
+    // Get distance between particles:
     var d = int(dist(particle.x, particle.y, this.x, this.y));
-    if (d < 1) {
+    if (d < 5) {
       this.v.x = 0;
       this.v.y = 0;
       particle.v.x = 0;
       particle.v.y = 0;
       return;
     }
-    // console.log(d);
-    var force = 4 / Math.pow(d, 2);
-    // console.log(force);
-    var forceConst = 50;
 
-    // get slope between particles:
+    var force = 1 / Math.pow(d, 2);
+    var forceConst = 8;
+
+    // Get slope between particles:
     var yDiff = - (this.y - particle.y);
     var xDiff = this.x - particle.x;
     var slope = yDiff / xDiff;
+    // actually we care about ratio of x to y:
     var arctan = atan(1 / slope);
 
     var angle;
 
-    // there is surely a cleverer way to do this:
+    // There is surely a cleverer way to do this:
     if (xDiff < 0 && yDiff < 0) {
       angle = 3*PI/2 + arctan;
     } else if (xDiff < 0 && yDiff > 0) {
@@ -56,11 +56,11 @@ class Particle {
   }
 
   move() {
+    // Basic physics engine:
     this.v.x += this.a.x;
     this.v.y += this.a.y;
 
     this.x += this.v.x;
     this.y += this.v.y;
-    // this.x += 1;
   }
 }
